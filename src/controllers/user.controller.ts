9b5be4665f5a706b090c421792a7dd08fc6cf6f9
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 21:11:08 +0100

    feature: signup

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,34 @@
 import { Request, Response } from "express"
+import { Api } from "../common/base/Api";
+import { BAD_REQUEST, CONFLICT } from "../common/const/codes";
+import { USERS_URL } from "../common/const/urls";
+import { User } from "../models/User";
+import { Validator } from "../common/base/Validator";
+import { v4 as uuidv4 } from 'uuid'
 
 // Sign Up
-const signup = (req: Request, res: Response) => {
-    res.json("Signed Up")
+const signup = async (req: Request, res: Response) => {
+    const user: User = req.body;
+    const errors = Validator.validateUser(user)
+
+    // Check if there any errors
+    if (Object.values(errors).filter(err => err).length) {
+        return res.status(BAD_REQUEST).json(errors)
+    }
+
+    //Check if user already exist in database
+    try {
+        const userResponse = await Api.get(USERS_URL, { email: user.email })
+        if (userResponse) {
+            return res.status(CONFLICT).json({ errors: 'User already exists' })
+        }
+        user.id = uuidv4()
+        await Api.post(USERS_URL, user);
+    } catch (err) {
+
+    }
+
+    return res.json("Signed Up")
 }
 // Sign In
 const signin = (req: Request, res: Response) => {
