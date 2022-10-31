060cde82c5e71f7633b4aa775a9d89df72bfbba6
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 19:09:47 +0100

    feature: basic api helper

---
+++ src/common/Api.ts
@@ -0,0 +1,24 @@
+import Axios, { AxiosResponse } from 'axios'
+
+export class Api {
+    private static baseUrl = 'http://localhost:3001/'
+
+    private static getUrl(url: string) {
+        return this.baseUrl + url
+    }
+
+    private static responseHandler(response: AxiosResponse) {
+        return response.data
+    }
+
+    static async get(url: string) {
+        const response = await Axios.get(this.getUrl(url))
+        return this.responseHandler(response)
+    }
+
+    static async post(url: string, data: any) {
+        const response = await Axios.post(this.getUrl(url), data)
+        return this.responseHandler(response)
+    }
+
+}
