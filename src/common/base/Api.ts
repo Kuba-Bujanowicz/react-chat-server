5649a22e34ca69566e4f242afe68d74ad1bf38cd
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 20:51:03 +0100

    feature: add params to request

--- src/common/base/Api.ts
+++ src/common/base/Api.ts
@@ -1,19 +1,20 @@
 import Axios, { AxiosResponse } from 'axios'
+import { RequestParams } from '../models/RequestParams';
 
 export class Api {
     private static baseUrl = 'http://localhost:3001'
 
     private static getUrl(url: string) {
-        return this.baseUrl + url
+        return this.baseUrl + url;
     }
 
     private static responseHandler(response: AxiosResponse) {
         return response.data
     }
 
-    static async get(url: string) {
-        const response = await Axios.get(this.getUrl(url))
-        return this.responseHandler(response)
+    static async get(url: string, params?: RequestParams) {
+        const response = await Axios.get(this.getUrl(url), { params })
+        return params ? this.responseHandler(response)[0] : this.responseHandler(response)
     }
 
     static async post(url: string, data: any) {
