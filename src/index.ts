7688f57c3c61da774637e1709e80eb96e7b77693
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 18:01:58 +0100

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import { createServer } from "http";
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { Routes } from "./routes";
 
 const port = process.env.PORT || 4000;
@@ -9,11 +10,13 @@ dotenv.config();
 
 const app: Express = express();
 
+app.use(cors())
+
 // Routes
 app.use(Routes.UserRoutes)
 
 const httpServer = createServer(app);
 
 httpServer.listen(4000, () => {
-    console.log(`⚡️ [server]: Server is running at https://localhost:${port}`);
+    console.log(`⚡️ [server]: Server is running at http://localhost:${port}`);
 });
