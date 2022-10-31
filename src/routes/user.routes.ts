fb646c75e44a9a392fbdbdbb30c38dd54855503c
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 18:15:42 +0100

    fix: typo

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import { User } from "../controllers/user.controller";
 const router = Router()
 
 router.post('/signup', User.signup)
-router.post('/signing', User.signin)
+router.post('/signin', User.signin)
 router.post('/logout', User.logout)
 
 export { router }
