dcff8d715f4dba975f1d984bd25a128d45b3c870
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 17:53:56 +0100

    feature: apply user routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Express } from "express";
 import { createServer } from "http";
 import dotenv from 'dotenv'
+import { Routes } from "./routes";
 
 const port = process.env.PORT || 4000;
 
 dotenv.config();
 
 const app: Express = express();
+
+// Routes
+app.use(Routes.UserRoutes)
+
 const httpServer = createServer(app);
 
 httpServer.listen(4000, () => {
