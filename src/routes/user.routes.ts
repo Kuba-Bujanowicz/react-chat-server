811f9adff9d21eee18d804a6d7b2f04d15f29ed6
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-10-31 17:42:01 +0100

    feature: add user routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import User from "../controllers/user.controller";
+
+const router = Router()
+
+router.post('/signup', User.signup)
+router.post('/signing', User.signin)
+router.post('/logout', User.logout)
+
+export default router
