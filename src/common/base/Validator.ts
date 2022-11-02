df903a62e82ba4e16a5029a0fb96eab7c61cfa61
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-02 14:04:59 +0100

    change: remove private

--- src/common/base/Validator.ts
+++ src/common/base/Validator.ts
@@ -6,7 +6,7 @@ export class Validator {
         return {} as T;
     }
 
-    private static validateEmail(email: string) {
+    static validateEmail(email: string) {
         const emailRegEx = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
 
         if (!email || email.trim() === '') {
@@ -18,7 +18,7 @@ export class Validator {
         }
     }
 
-    private static validateText(text: string, errorName?: string) {
+    static validateText(text: string, errorName?: string) {
         if (!text || text.trim() === '') {
             return `${errorName || 'Field'} is required`
         }
