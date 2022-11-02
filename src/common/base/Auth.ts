8365b1e0b62fdd42968fa3d5d6768705b0d2a1ee
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-02 12:57:59 +0100

    feature: token helper

---
+++ src/common/base/Auth.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+
+export class Auth {
+    static generateToken(payload: string) {
+        return jwt.sign({data: payload}, process.env.TOKEN_SECRET as string, { expiresIn: 60 })
+    }
+
+    static verifyToken(token: string) {
+        return jwt.verify(token, process.env.TOKEN_SECRET as string)
+    }
+}
