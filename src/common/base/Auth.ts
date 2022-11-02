4e2882836e004f902ceedf3e733ea4c8b4a10edc
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-02 13:17:23 +0100

    change: expires

--- src/common/base/Auth.ts
+++ src/common/base/Auth.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export class Auth {
     static generateToken(payload: string) {
-        return jwt.sign({data: payload}, process.env.TOKEN_SECRET as string, { expiresIn: 60 })
+        return jwt.sign({data: payload}, process.env.TOKEN_SECRET as string, { expiresIn: '1h' })
     }
 
     static verifyToken(token: string) {
