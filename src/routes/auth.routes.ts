c2654a3a539a0f3be38ad222d2a74e9b8b82696f
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-03 21:02:42 +0100

    fix: add middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router()
 
 router.post('/signup', AuthController.signup)
 router.post('/signin', AuthController.signin)
-router.post('/logout', AuthController.logout)
-router.post('/deleteProfile', AuthController.deleteAccount)
+router.post('/logout', AuthMiddleware.authenticateToken, AuthController.logout)
+router.post('/deleteProfile', AuthMiddleware.authenticateToken, AuthController.deleteAccount)
 
 export { router }
