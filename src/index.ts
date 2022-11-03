f28a45fa326b993a532ea658eb12fdacad1e4a79
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-03 20:08:59 +0100

    feature: add cookie parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { createServer } from "http";
 import dotenv from 'dotenv'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { Routes } from "./routes";
 
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ dotenv.config();
 const app: Express = express();
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
 // Routes
