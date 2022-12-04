947ce4f80d28f4e6dd05a34b0a08fd81b557a649
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-04 22:41:43 +0100

    change: refactor route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { Api } from '../common/base/Api';
-import { BAD_REQUEST, CONFLICT, FORBIDDEN, NOT_FOUND, OK, PERMANENT_REDIRECT, UNAUTHORIZED } from '../common/const/codes';
+import { BAD_REQUEST, CONFLICT, FORBIDDEN, OK, UNAUTHORIZED } from '../common/const/codes';
 import { USERS_URL } from '../common/const/urls';
 import { User } from '../models/User';
 import { Validator } from '../common/base/Validator';
@@ -8,14 +9,11 @@ import { v4 as uuidv4 } from 'uuid';
 import { Auth } from '../common/base/Auth';
 import { JwtPayload } from 'jsonwebtoken';
 import { SignUpUser } from '../models/SignUpUser';
-import { Password } from '../common/base/Password';
+import { SignInUser } from '../models/SignInUser';
 
 // Sign Up
 const signup = async (req: Request, res: Response) => {
-  // Get user from request
   const user: SignUpUser = req.body;
-
-  // Validate user's credentials
   const errors = Validator.validateSignUpUser(user);
 
   // Check if there any errors
@@ -40,7 +38,7 @@ const signup = async (req: Request, res: Response) => {
     id: uuidv4(),
     email: user.email,
     name: user.name,
-    password: await Password.hashPassword(user.password),
+    passwordHash: await bcrypt.hash(user.password, 10),
   };
 
   await Api.post(USERS_URL, newUser);
@@ -59,25 +57,24 @@ const signup = async (req: Request, res: Response) => {
 };
 // Sign In
 const signin = async (req: Request, res: Response) => {
-  const email: string = req.body.email;
-  const errors = { email: Validator.validateEmail(email) };
+  const user: SignInUser = req.body;
+  const errors = Validator.validateSignInUser(user);
 
   // Check if there any errors
   if (Object.values(errors).filter((err) => err).length) {
     return res.status(BAD_REQUEST).json(errors);
   }
 
-  //Check if user already exist in database
-  const user: User = await Api.get(USERS_URL, { email });
-  if (!user) {
-    return res.status(NOT_FOUND).json({ email: 'User not found' });
+  //Check if user already exist in database and verify password
+  const userResponse: User = await Api.get(USERS_URL, { email: user.email });
+  const match = await bcrypt.compare(user.password, userResponse.passwordHash);
+
+  if (!userResponse || !match) {
+    return res.status(BAD_REQUEST).json({ errorMessage: 'Wrong email or password' });
   }
 
-  user.isActive = true;
-  await Api.put(USERS_URL, user, user.id);
-
   //Generate jwt token
-  const token = Auth.generateToken(user.id);
+  const token = Auth.generateToken(userResponse.id);
 
   res.cookie('token', token, {
     httpOnly: true,
@@ -91,18 +88,7 @@ const signin = async (req: Request, res: Response) => {
 
 // Logout
 const logout = async (req: Request, res: Response) => {
-  const token: string = req.cookies.token;
-  const decodedToken = Auth.decodeToken(token) as JwtPayload;
-
-  //Change user isActive to false
-  try {
-    const user: User = await Api.get(USERS_URL, { id: decodedToken.data });
-    user.isActive = false;
-    await Api.put(USERS_URL, user, user.id);
-    res.clearCookie('token');
-  } catch {
-    return res.status(BAD_REQUEST).json('Cannot logout');
-  }
+  res.clearCookie('token');
 
   return res.status(OK).send('User logout');
 };
