6ec62144307d8ad7f8636fd2b06529280abe5ccf
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-07 02:31:37 +0100

    change: rename path

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,22 @@
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  passwordHash: string;
-}
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('User', userSchema);
--- src/types/UserPublic.ts
+++ src/types/UserPublic.ts
@@ -1,4 +1,4 @@
 export interface UserPublic {
-  id: string;
+  email: string;
   name: string;
 }
