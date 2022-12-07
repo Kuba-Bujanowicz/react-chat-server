e5b7f34d0e568d4f0a03e0c4c893f043e5cbc22b
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-07 02:30:47 +0100

    change: prettier

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,20 +1,21 @@
-import { NextFunction, Request, Response } from "express";
-import { Auth } from "../common/base/Auth";
-import { FORBIDDEN, UNAUTHORIZED } from "../common/const/codes";
+import { NextFunction, Request, Response } from 'express';
+import { Auth } from '../common/base/Auth';
+import { FORBIDDEN, UNAUTHORIZED } from '../common/const/codes';
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token: string = req.cookies.token;
+  const token: string = req.cookies.token;
+  console.log(req.cookies.token);
 
-    if (!token) return res.status(UNAUTHORIZED).json('Missing authorization token');
+  if (!token) return res.status(UNAUTHORIZED).json('Missing authorization token');
 
-    try {
-        Auth.verifyToken(token);
-        next()
-    } catch (error) {
-        return res.status(FORBIDDEN).json('Invalid authorization token')
-    }
-}
+  try {
+    Auth.verifyToken(token);
+    next();
+  } catch (error) {
+    return res.status(FORBIDDEN).json('Invalid authorization token');
+  }
+};
 
 export const AuthMiddleware = {
-    authenticateToken
-}
+  authenticateToken,
+};
