eae0c910de20050ac2baa4a247aad9e90ba227bf
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-09 19:59:30 +0100

    feature: add email verification middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 import { Auth } from '../common/base/Auth';
-import { FORBIDDEN, UNAUTHORIZED } from '../common/const/codes';
+import UserModel from '../models/User';
+import { NOT_FOUND, FORBIDDEN, UNAUTHORIZED } from '../common/const/codes';
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const token: string = req.cookies.token;
@@ -15,6 +17,23 @@ const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const authenticateEmail = async (req: Request, res: Response, next: NextFunction) => {
+  const token: string = req.cookies.token;
+  const decodedToken = Auth.decodeToken(token) as JwtPayload;
+  const user = await UserModel.findById(decodedToken.data);
+
+  if (!user) {
+    return res.status(NOT_FOUND).send('User not found');
+  }
+
+  if (!user.isVerified) {
+    return res.status(FORBIDDEN).send('Email not verified');
+  }
+
+  next();
+};
+
 export const AuthMiddleware = {
   authenticateToken,
+  authenticateEmail,
 };
